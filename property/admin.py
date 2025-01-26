53dce43737c9c2f62b2327018e9b8437c0d03599
VladimirZai <55639619+VladimirZai@users.noreply.github.com>
2025-01-26 22:52:16 +0100

    setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Like
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner', )
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', )
     list_editable = ('new_building', )
     list_filter = ('new_building', 'rooms_number', 'has_balcony', )
+    raw_id_fields = ('liked_by', )
 
 
 class ComplaintAdmin(admin.ModelAdmin):
--- property/models.py
+++ property/models.py
@@ -48,9 +48,16 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", related_name="liked_flats")
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+
+class Like(models.Model):
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Complaint(models.Model):
